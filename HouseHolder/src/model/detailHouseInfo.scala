d462b5485bd10a65a283dcc3fd33ddc23e0abc7f
nishida-takuro <nishida-takuro@192.168.100.101>
2017-01-25 11:47:15 +0900

    first commit

---
+++ HouseHolder/src/model/detailHouseInfo.scala
@@ -0,0 +1,27 @@
+package model
+
+//詳細情報クラス
+class detailHouseInfo {
+  //建物名
+  var buildingName: String = _   
+  //階数
+  var floor: String = _
+  //賃料
+  var rant: String = _
+  //管理費
+  var AdministrativeExpense: String = _
+  //敷金
+  var deposit: String = _
+  //礼金
+  var gratuityFee: String = _
+  //保証金
+  var securityFee: String = _
+  //敷引金,償却
+  var insoleTrigger: String = _
+  //間取り
+  var layout: String = _
+  //専有面積
+  var areaSize: String = _
+  //詳細ページURL
+  var detailPageURL: String = _
+}
