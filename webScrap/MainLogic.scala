f58be84512e7428e579178f805d52799c857060d
takutakutakujiro <nishida-takuro@nishida-takuro-no-MacBook-Pro.local>
2017-04-26 01:34:56 +0900

    sumo
    行数削減リファクタリング

--- webScrap/MainLogic.scala
+++ webScrap/MainLogic.scala
@@ -36,89 +36,58 @@ object MainLogic extends App{
 	}
 
 	def createUrlList(maxPageNo : Int): List[String] = {
-		var listUrl = List.empty[String]
+		import collection.mutable.ListBuffer
+		var b = new ListBuffer[String]
 		for(iPage <- 1 to maxPageNo){
 			var createdUrl = createUrl(iPage.toString)
 			println("[SUMO][世田谷区][URL][" + iPage + "]：" + createdUrl)
-			listUrl = createdUrl :: listUrl
+			b += createdUrl
 		}
-		listUrl
+		b.toList
 	}
 
 	def createJsonFile (list : List[String]) {
-		val jsonStrList = createJsonString(list)
-		val output = new File("./json/output_sumo.json")
-	    val file = new PrintWriter(output)
-
-		jsonStrList.foreach { m => 
-			file.println(getDefIndexJson + "\n" + m)
-		}
-
+		val jsonStr = createJsonString(list)
+	    val file = new PrintWriter(new File("./json/output_sumo.json"))
+	    file.print(jsonStr)
 		file.close
 	}
 
 	def createUrl (pageNo : String) = urlstr + "pnz1" + pageNo + ".html"
 
-	def createJsonString (list : List[String]): List[String] = {
-		var docList = List[String]()
+	def createJsonString (list : List[String]): String = {	
+		import collection.immutable.ListMap
+		var b = new StringBuilder()
 		list.par.map { url =>
-			val jsoup = start(url, ".property_unit-content")
-			var title, detailUrl, img, minPrice, maxPrice, address, station, minSpace, maxSpace, roomLayout : String = ""
-			 
+			val jsoup = start(url, ".property_unit-content")			 
 			jsoup.par.map { m =>
 				//タイトル、詳細画面url、画像、販売価格、所在地、駅、土地面積、間取り
-				title = getTitle(m)
-				detailUrl = getDetailUrl(m)
-				img = getImg(m)
-				minPrice = getMinPrice(m)
-				maxPrice = getMaxPrice(m)
-				address = getAddress(m)
-				station = getStation(m)
-				minSpace = getMinSpace(m)
-				maxSpace = getMaxSpace(m)
-				roomLayout = getRoomLayout(m)
+				val docJson = ListMap[String, String](
+					"title"      -> getTitle(m),
+					"detail_url" -> getDetailUrl(m),
+					"img" 		 -> getImg(m),
+					"minPrice"   -> getMinPrice(m),
+					"maxPrice"   -> getMaxPrice(m),
+					"address"    -> getAddress(m),
+					"station" 	 -> getStation(m),
+					"minSpace" 	 -> getMinSpace(m),	
+					"maxSpace" 	 -> getMaxSpace(m),	
+					"roomLayout" -> getRoomLayout(m)
+				)
 
-				var b = new StringBuilder()
-				b ++= "{"
-				b ++= getDocJson("title", title)
-				b ++= ","
-				b ++= getDocJson("detail_url", detailUrl)
-				b ++= ","
-				b ++= getDocJson("img", img)
-				b ++= ","
-				b ++= getDocJson("minPrice", minPrice)
-				b ++= ","
-				b ++= getDocJson("maxPrice", maxPrice)
-				b ++= ","
-				b ++= getDocJson("address", address)
-				b ++= ","
-				b ++= getDocJson("station", station)
-				b ++= ","
-				b ++= getDocJson("minSpace", minSpace)	
-				b ++= ","
-				b ++= getDocJson("maxSpace", maxSpace)	
-				b ++= ","
-				b ++= getDocJson("roomLayout", roomLayout)															
-				b ++= "}"
+				println("\n======")
 
-				docList :+= b.toString
-
-				println
-				println("======")
-				println("title" + "：" + title)
-				println("detailUrl" + "：" + detailUrl)
-				println("img" + "：" + img)
-				println("minPrice" + "：" + minPrice)
-				println("maxPrice" + "：" + maxPrice)
-				println("address" + "：" + address)
-				println("station" + "：" + station)
-				println("minSpace" + "：" + minSpace)
-				println("maxSpace" + "：" + maxSpace)
-				println("roomLayout" + "：" + roomLayout)
-				//println(m.toString)	
+				b ++= getDefIndexJson
+				b ++= "\n{"
+				docJson.foreach { case(key, value) =>
+					b ++= getDocJson(key, value)
+					if (key != "roomLayout") b ++= ","
+					println(key + "：" + value)	
+				}
+				b ++= "}\n"
 			}
 		}
-		docList
+		b.toString
 	}
 
 	def optCheck(o: Option[Match]) = {
@@ -134,47 +103,17 @@ object MainLogic extends App{
     	optCheck(doc)
     }
 
-    def getTitle (e : Element) = {
-    	val regex = """<h2 class="property_unit-title">.*<a.*>(.*)</a></h2>"""
-    	getItem(e.toString, regex)
+    def getTitle      (e : Element): String = getItem(e.toString, """<h2 class="property_unit-title">.*<a.*>(.*)</a></h2>""")
+    def getDetailUrl  (e : Element): String = getItem(e.toString, """<h2 class="property_unit-title">.*<a href="(.*)" target""")
+    def getImg        (e : Element): String = getItem(e.toString, """rel="(.*)" alt""").replaceAll("amp;","")
+    def getAddress    (e : Element): String = getItem(e.toString, """所在地\n.*\n.*\n\s+?(\S.*)""")
+    def getStation    (e : Element): String = getItem(e.toString, """沿線・駅\n.*\n.*\n\s+?(\S.*)""")	
+    def getMinSpace   (e : Element): String = getItem(e.toString, """土地面積\n.*\n.*\n\s+?(\S.*)m""")
+	def getRoomLayout (e : Element): String = getItem(e.toString, """間取り\n.*\n.*\n\s+?(\S.*)""")
+    def getMaxSpace   (e : Element): String = {
+    	val result = getItem(e.toString, """土地面積\n.*\n.*\n.*\n.*</sup>[～|・](.*)m""")
+    	if (isNone(result)) return getMinSpace(e) else result
     }
-
-    def getDetailUrl (e : Element) = {
-    	val regex = """<h2 class="property_unit-title">.*<a href="(.*)" target"""
-    	getItem(e.toString, regex)	
-    }
-
-    def getImg (e : Element) = {
-    	val regex = """rel="(.*)" alt"""
-    	getItem(e.toString, regex).replaceAll("amp;","")	    	
-    }
-
-    def getAddress (e : Element) = {
-    	val regex = """所在地\n.*\n.*\n\s+?(\S.*)"""
-    	getItem(e.toString, regex)	
-    }
-
-    def getStation (e : Element) = {
-    	val regex = """沿線・駅\n.*\n.*\n\s+?(\S.*)"""
-    	getItem(e.toString, regex)	
-    }
-
-    def getMinSpace (e : Element) = {
-    	val regex = """土地面積\n.*\n.*\n\s+?(\S.*)m"""
-    	getItem(e.toString, regex)
-    }
-
-    def getMaxSpace (e : Element): String = {
-    	val regex = """土地面積\n.*\n.*\n.*\n.*</sup>[～|・](.*)m"""
-    	val result = getItem(e.toString, regex)
-    	if (isNone(result)) return getMinSpace(e)
-    	result
-    }
-
-	def getRoomLayout (e : Element): String = {
-    	val regex = """間取り\n.*\n.*\n\s+?(\S.*)"""
-    	getItem(e.toString, regex)
-    }    
 
     def getMinPrice (e : Element): String = {
     	val regex1 = """dottable-value">(.*[万|億]).*～.*</span>"""
